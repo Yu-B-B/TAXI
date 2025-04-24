4d07817a3ae537085a83681bf65bb05f1c8cb3a6
XF <xialuke98@gmail.com>
2025-04-24 16:04:00 +0800

    boss端控制【司机】与【车辆】的绑定与解绑

---
+++ api-boss/src/main/java/com/ybb/controller/DriverCarBindingRelationshipController.java
@@ -0,0 +1,34 @@
+package com.ybb.controller;
+
+import com.ybb.dto.DriverCarBindingRelationship;
+import com.ybb.dto.ResponseResult;
+import com.ybb.service.DriverCarBindingRelationshipService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/driver-car-binding-relationship")
+public class DriverCarBindingRelationshipController {
+
+    @Autowired
+    private DriverCarBindingRelationshipService driverCarBindingRelationshipService;
+
+    /**
+     * 管理端控制【司机】与【车辆】绑定关系
+     *
+     * @param driverCarBindingRelationship
+     * @return
+     */
+    @PostMapping("/bind")
+    public ResponseResult bind(@RequestBody DriverCarBindingRelationship driverCarBindingRelationship){
+        return driverCarBindingRelationshipService.bind(driverCarBindingRelationship);
+    }
+
+    @PostMapping("/unbind")
+    public ResponseResult unbind(@RequestBody DriverCarBindingRelationship driverCarBindingRelationship){
+        return driverCarBindingRelationshipService.unbind(driverCarBindingRelationship);
+    }
+}
---
+++ api-boss/src/main/java/com/ybb/service/DriverCarBindingRelationshipService.java
@@ -0,0 +1,24 @@
+package com.ybb.service;
+
+import com.ybb.dto.DriverCarBindingRelationship;
+import com.ybb.dto.ResponseResult;
+import com.ybb.feign.DriverServiceFeign;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DriverCarBindingRelationshipService {
+
+    @Autowired
+    private DriverServiceFeign driverServiceFeign;
+
+
+    public ResponseResult bind(DriverCarBindingRelationship driverCarBindingRelationship){
+        return driverServiceFeign.bind(driverCarBindingRelationship);
+    }
+
+    public ResponseResult unbind(DriverCarBindingRelationship driverCarBindingRelationship) {
+
+        return driverServiceFeign.unbind(driverCarBindingRelationship);
+    }
+}
